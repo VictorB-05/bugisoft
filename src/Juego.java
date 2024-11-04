0463fdce8bd4b4c854aa8cb8b86d0dd1a24c1afe
victor <victorbejar2005@gmail.com>
2024-11-04 20:58:14 +0100

    javax

--- src/Juego.java
+++ src/Juego.java
@@ -1,3 +1,9 @@
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name="Juego")
+@XmlType(propOrder={"nombre, libros"})
 public class Juego {
     //Añadir datos de juegos
+
 }
